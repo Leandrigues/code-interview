f5286d230956e60342504dca3b5afa9b9100cde5
Leandro Rodrigues <leandro.rodrigues.silva99@gmail.com>
2021-04-21 14:19:05 -0300

    Add solutions

---
+++ longestDistinct.py
@@ -0,0 +1,19 @@
+# DailyCoding 13
+
+def solution(s, k):
+  longestSubstringLength = -1
+  substrings = []
+  for i in range(len(s)):
+    substring = s[i]
+    distinct = 1
+    for j in range(i+1, len(s)):
+      if s[j] not in substring:
+        distinct += 1
+      if distinct <= k:
+        substring += s[j]
+        longestSubstringLength = max(longestSubstringLength, len(substring))
+        substrings.append(substring)
+      else:
+        break
+  print(max(substrings, key=len))
+  return longestSubstringLength
